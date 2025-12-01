0f091312b80f56b2a364118362e427a0c2a72ab5
Bekzat <baymukach@gmail.com>
2025-12-01 11:12:35 +0500

    Commit

--- baymukach_gmail_com.php
+++ baymukach_gmail_com.php
@@ -1,5 +1,4 @@
 <?php
-// Проверка: x и y - неотрицательные целые (0,1,2,3,...)
 function valid($v) {
     return preg_match('/^\d+$/', $v);
 }
@@ -14,12 +13,8 @@ function gcd_php($a, $b) {
 }
 
 function lcm_php($x, $y) {
-    if ($x == 0 || $y == 0) {
-        return "0";
-    }
-    $g = gcd_php($x, $y);
-    // чтобы уменьшить шанс переполнения, сначала делим
-    return (string)(int)(($x / $g) * $y);
+    if ($x == 0 || $y == 0) return "0";
+    return (string)(($x / gcd_php($x, $y)) * $y);
 }
 
 $x = $_GET['x'] ?? null;
